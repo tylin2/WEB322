db868a389fc0ff8befb2ea829359764a71dbbec9
tylin2 <tylin2@myseneca.ca>
2020-03-29 20:10:59 -0400

    add user files

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express = require("express");  
 const exphbs= require("express-handlebars");
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const generalController =require("./controllers/general");
+const productController=require("./controllers/product");
+const userController=require("./controllers/user");
+
 require('dotenv').config({path:"./config/keys.env"}); //load environment variable file
 
 const app = express();
 
 app.engine("handlebars",exphbs());
 app.set("view engine", "handlebars");
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static("public"));
-
-//load controller
-const generalController =require("./controllers/general");
-const productController=require("./controllers/product");
-
 app.use("/",generalController);
 app.use("/products",productController);
+app.use("/users",userController);
 
+mongoose.connect(process.env.MONGO_DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{
+    console.log(`Connected to MONGODB~`);
+})
+.catch(err=>console.log(`Error occured : ${err}`));
 
 
 const PORT=process.env.PORT;
---
+++ controllers/user.js
@@ -0,0 +1,125 @@
+const express = require('express')
+const router = express.Router();
+
+router.get("/registration",(req,res)=>{
+
+    res.render("users/registration",{ //add customers
+        title:"registration",
+        headingInfo: "Registration"       
+    });
+
+});
+
+//add new app.post
+router.post("/registration",(req,res)=>{
+        
+    const errorMessages = [];
+        
+        if(req.body.fullName=="")
+        {    
+                        
+                errorMessages.push("! Enter Your Name");
+        }
+
+        if(req.body.email=="")        {
+                
+                errorMessages.push("! Enter Your email");
+        }
+
+        if(req.body.password=="")
+        {
+                errorMessages.push("! Enter Your password");
+        }
+        var reg = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[~!@#$%^&*()_+`\-={}:";'<>?,.\/]).{6,12}$/;                    
+        if (!reg.test(req.body.password)) {
+                errorMessages.push("! Password must consist of 6-12 letters, numbers, and special symbols");
+        }
+
+        if(req.body.password!=req.body.password2)
+        {
+                errorMessages.push("! Passwords are not match");
+        }
+       
+        if(errorMessages.length > 0 )
+        {
+                
+            res.render("users/registration",{ 
+                title : "registration",
+                headingInfo: "Registration",                        
+                errors : errorMessages,
+                fullName:req.body.fullName,
+                email:req.body.email,
+                password:req.body.password,
+                password2:req.body.password2               
+                });
+
+        }else{
+                const {fullName, email}=req.body;
+                const sgMail = require('@sendgrid/mail');
+                sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+                const welcome = {
+                  to: `${email}`,
+                  from: `s88432000@gmail.com`,
+                  subject: `Welcome! Confirm Your Email`,
+                  html: 
+                  `Thanks for signing up! ${fullName} <br> 
+                   Your username is ${email}.<br>
+                  `,
+                };
+                sgMail.send(welcome)
+                .then(()=>{
+                        res.redirect("/");
+                })
+                .catch(err=>{
+                        console.log(`Error ${err}`)
+                })
+                
+        }
+
+   
+
+});
+
+router.get("/login",(req,res)=>{
+       
+        res.render("users/login",{ //add customers
+                title:"login",
+                headingInfo: "Login"       
+        });
+            
+});
+
+router.post("/login",(req,res)=>{
+
+    const errorMessages = [];
+        
+        if(req.body.email=="")
+        {
+                errorMessages.push("! Enter Your email");
+        }
+
+        if(req.body.password=="")
+        {
+                errorMessages.push("! Enter Your password");
+        }
+
+        if(errorMessages.length >0 )
+        {
+                res.render("users/login",{ 
+                        title : "login",
+                        headingInfo: "Login",                        
+                        errors : errorMessages               
+                });
+
+        } else {
+                
+                res.render("users/login",{ //add customers
+                        title : "login",
+                        headingInfo: "Login",                        
+                        successMessage : ``
+                       
+                });
+        }
+});
+
+module.exports=router;
